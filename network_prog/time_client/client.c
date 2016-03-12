7ec1444af63839bd7b2fa654bcb8a0e936c5fafd
Jesse <scalpel.etf@gmail.com>
2016-03-12 14:44:54 -0600

    get time tist

--- network_prog/time_client/client.c
+++ network_prog/time_client/client.c
@@ -1,4 +1,4 @@
-#include "socket_include.h"
+#include "socket_includes.h"
 #include "stdio.h"
 #include "string.h"
 
@@ -6,6 +6,8 @@ int main(int argc, *char argv[])
 {
 	int sockfd;
 	struct sockaddr servaddr;//
+	char buff[128] = {0};
+	int bytes;
 	
 	sockfd = socket(AF_INET,//IPV4 internet protocols
 			SOCK_STREAM,//通信语义使用STREAM类型
@@ -16,7 +18,7 @@ int main(int argc, *char argv[])
 		return -1;
 	}
 
-	bzero(servaddr, sizeof(servaddr));//wirte zero
+	bzero(&servaddr, sizeof(servaddr));//wirte zero
 	servaddr.sin_family = AF_INET;//使用IPv4协议
 	servaddr.sin_ddr.s_addr = inet_addr("127.0.0.1");//指定IP地址
 	servaddr.sin_port = htons(8888);//指定端口
@@ -26,6 +28,15 @@ int main(int argc, *char argv[])
 		perror("connect error");
 		return -2;
 	}
-
+		
+	bytes = read(sockfd,buff,sizeof(buf));
+	if(bytes < 0)
+	{
+		printf("read error\n");
+		return -3;
+	}
+	
+	printf("Time : %s",buff);
+	close(sockfd);
 
 }

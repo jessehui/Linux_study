1626a24bda4bd85d41b65a0e95c5758adb305a67
Jesse <scalpel.etf@gmail.com>
2016-03-13 13:14:46 -0500

    server test

---
+++ network_prog/time_server/server.c
@@ -0,0 +1,70 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+
+#include "sys/socket.h"
+#include "sys/types.h"
+#include "netinet/in.h"
+
+#include "unistd.h"
+#include "fcntl.h"
+
+//最大监听队列
+#define MAX_LISTEN_QUE 5
+
+int main(int argc, char * argv[])
+{
+	int sockfd, listenfd;//sockfd通信套接字， listenfd监听套接字
+	struct sockaddr_in server, client;
+	int ret_bind;
+	char buff[128] = {0};
+	int len;
+	time_t timep;//#define time_t int
+	
+	listenfd = socket(AF_INET,SOCK_STREAM,0);
+	if(listenfd < 0)
+	{
+		printf("Create socket fail\n");
+		return -1;
+	}
+
+	bzero(&server,sizeof(server));//地址后的size个bytes全为0
+	server.sin_family = AF_INET;//IPV4协议族
+	server.sin_port = htons(8888);//hostshort from host tyte order to network byte order
+	server.sin_addr.s_addr = htonl(INADDR_ANY);//绑定服务器上所有网络接口
+
+	len = sizeof(struct sockaddr);	
+	//绑定套接字
+	ret_bind = bind(listenfd,(struct sockaddr *)&server,len);
+	//(struct sockaddr *)&serve 强制转换成sockaddr的指针
+	
+	if(ret_bind < 0)
+	{
+		printf("Bind Error\n");
+		return -2;
+	}
+	
+	listen(listenfd,MAX_LISTEN_QUE);
+	
+	while(1)
+	{
+	sockfd = accept(listenfd,(struct sockaddr *)&client,&len);
+	if(sockfd < 0)
+	{
+		printf("Accept Error\n");
+		return -3;
+	}
+
+	//读时间
+	timep = time(NULL);
+	
+	//sprintf(buff,"%s",ctime(&timep));
+	snprintf(buff,sizeof(buff),"%s",ctime(&timep));
+	printf("buff = %s",buff);
+	write(sockfd,buff,strlen(buff));
+	printf("Bytes Server = %d\n",sizeof(buff));	
+	close(sockfd);
+	}
+	return 0;
+}	
+
---
+++ network_prog/time_server/server_example.c
@@ -0,0 +1,76 @@
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+
+#include <sys/types.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+
+
+#include <unistd.h>
+#include <fcntl.h>
+
+
+
+#define MAX_LISTEN_QUE 5
+
+
+
+
+int main(int argc, char *argv[])
+{
+	int listenfd, sockfd, opt = 1;
+	struct sockaddr_in server, client;
+	char buf[200];
+	socklen_t len;
+	int timep;
+	int ret;
+	
+	listenfd = socket(AF_INET, SOCK_STREAM, 0);
+  if(listenfd < 0){
+			perror("Create socket fail.");
+			return -1;
+  }	
+	
+	if((ret = setsockopt(listenfd, SOL_SOCKET, SO_REUSEADDR, &opt, sizeof(opt))) < 0){
+  		perror("Error, set socket reuse addr failed");	
+  		return -1;
+  }
+	
+	bzero(&server, sizeof(server));
+  server.sin_family = AF_INET;
+  server.sin_port   = htons(8888);
+  server.sin_addr.s_addr  = htonl(INADDR_ANY);
+	
+	len = sizeof(struct sockaddr);
+  if(bind(listenfd, (struct sockaddr *)&server, len)<0){
+			perror("bind error.");
+      return -1;
+  }
+	
+	listen(listenfd, MAX_LISTEN_QUE);
+	
+	while(1){
+		sockfd = accept(listenfd, (struct sockaddr *)&client, &len);
+	  if(sockfd < 0){
+				perror("accept error.");
+				return -1;
+	  }
+	  
+		timep = time(NULL);
+		snprintf(buf, sizeof(buf), "%s", ctime(&timep));
+		write(sockfd, buf, strlen(buf));
+		printf("Bytes:%d\n", strlen(buf));
+		printf("sockfd=%d\n", sockfd);
+		close(sockfd);
+	}
+	return 0;
+	
+}
+
+
+
+
+

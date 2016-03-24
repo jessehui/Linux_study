53d1594ca77b7de8f14c0fc9cfd88ced276df493
Jesse <scalpel.etf@gmail.com>
2016-03-24 10:38:15 -0500

    header file test

--- network_prog/multiple_process/server_zombie.c
+++ network_prog/multiple_process/server_zombie.c
@@ -1,20 +1,4 @@
-#include "stdio.h"
-#include "stdlib.h"
-#include "string.h"
-#include "time.h"
-
-#include "sys/socket.h"
-#include "sys/types.h"
-#include "netinet/in.h"
-
-#include "unistd.h"
-#include "fcntl.h"
-#include <sys/socket.h>
-#include <netinet/in.h>
-#include <arpa/inet.h>
-
-#include <sys/types.h>
-#include <sys/wait.h>
+#include "../socket_includes.h"
 
 
 //最大监听队列
---
+++ network_prog/socket_includes.h
@@ -0,0 +1,17 @@
+#include "stdio.h"
+#include "stdlib.h"
+#include "string.h"
+#include "time.h"
+
+#include "sys/socket.h"
+#include "sys/types.h"
+#include "netinet/in.h"
+
+#include "unistd.h"
+#include "fcntl.h"
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <arpa/inet.h>
+
+#include <sys/types.h>
+#include <sys/wait.h>

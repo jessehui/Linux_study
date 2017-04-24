714926dbc707d0047d23e5e4dbced1ac7e2fb6e2
Jesse <scalpel.etf@gmail.com>
2017-04-24 11:55:35 -0500

    thread_test

---
+++ process_comm/L1/thread_test.c
@@ -0,0 +1,53 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <pthread.h>
+
+int thread_inter = 0;
+
+void* fun(void *var)	//child thread
+{
+	int j;
+	while(thread_inter == 0);
+	for(j = 0; j < 10; j++)
+	{
+		usleep(100);
+		printf("this is fun j = %d \n", j);
+	}
+
+}
+
+
+int main()	//main thread
+{
+	int i;
+	char str[] = "hello Linux\n";
+
+	pthread_t tid;
+	int ret;
+	ret = pthread_create(&tid, NULL, fun, (void*)str);
+
+	if(ret < 0)
+	{
+		printf("create thread failure\n");
+		return -1;
+	}
+
+	for(i = 0; i < 10; i++)
+	{
+		usleep(100);
+		printf("this is main i = %d\n",i);
+	}
+
+	thread_inter = 1;
+	while(1);
+	return 0;
+}
+
+
+
+
+
+
+
+
+
